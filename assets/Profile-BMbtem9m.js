7d8bb693c857b565fdd746a7ced1a71635b94ab4
Krishna Therokar <krishnatherokar@gmail.com>
2025-02-12 21:27:55 +0530

    Updates

--- assets/Profile-BMbtem9m.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-B-XIlQkP.js";const o=()=>r.jsx("div",{children:"Profile"});export{o as default};
--- assets/ViewForm-D6ybNsBu.js
+++
@@ -1,1 +0,0 @@
-import{u as r,j as a}from"./index-B-XIlQkP.js";const i=()=>{const{id:s}=r();return a.jsxs("div",{children:["id using useParams: ",s]})};export{i as default};
