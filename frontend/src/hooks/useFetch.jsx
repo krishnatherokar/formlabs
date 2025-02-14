de63a12d59202361d6e3d0941b88514abec51e87
Krishna Therokar <krishnatherokar@gmail.com>
2025-02-14 19:35:18 +0530

    feat: add axios

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,22 +1,45 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-const useFetch = (url) => {
-  const [data, setdata] = useState("Loading");
+const useFetch = (url, { body = null, ...options} = {}, method = "GET") => {
+  const [state, setstate] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        let response = await fetch(url);
-        if (!response.ok) throw new Error(response.statusText);
-        let data = await response.json();
-        setdata(data);
-      } catch (err) {
-        setdata(err.message);
+    const controller = new AbortController();
+    const config = {
+      ...options,
+      signal: controller.signal,
+      headers: {
+        "Content-Type": "application/json",
+        ...(options.headers || {}),
       }
     }
-    fetchData();
+
+    async function fetchData() {
+      setstate({ data: null, loading: true, error: null });
+      try {
+        let response = method == "GET"? await axios.get(url, config): await axios.post(url, body, config);
+        let data = response.data;
+        setstate({ data: data, loading: false, error: null });
+      } catch (err) {
+        setstate({
+          data: null,
+          loading: false,
+          error: err.response
+            ? `Error ${err.response.status}: ${err.response.data}`
+            : err.message,
+        });
+      }
+    }
+
+    if (url) fetchData();
+    return () => controller.abort();
   }, [url]);
 
-  return data;
+  return state;
 };
 export default useFetch;
