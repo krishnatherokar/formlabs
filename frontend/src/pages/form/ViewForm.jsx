34fc411a6481a61596d60df757c5098078a137b9
Krishna Therokar <krishnatherokar@gmail.com>
2025-02-16 21:41:23 +0530

    refactor: change context import path

--- frontend/src/pages/form/ViewForm.jsx
+++ frontend/src/pages/form/ViewForm.jsx
@@ -3,27 +3,28 @@ import { useContext, useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import FormBody from "../../components/form/FormBody";
 import { deleteLocalAns } from "../../utils/handleLocalSync";
-import { UserContext } from "../../context/userContext";
+import { UserContext } from "../../context/UserContext";
 
 const ViewForm = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
   // get the current user
-  const {user} = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-  const [url, setUrl] = useState(null)
+  const [url, setUrl] = useState(null);
 
   // get the form data
   const { data, loading, error } = useFetch(url);
 
   useEffect(() => {
     const r = user?.responses?.find((r) => r.formId === id);
-    // if the user has already responded to this form, redirect to the response page
     if (r) {
+      // if the user has already responded to this form, redirect to the response page
       deleteLocalAns(r.formId);
       navigate(`/form/r/${r.responseId}`);
     } else {
+      // in other cases, set the url to load the form
       setUrl(`${import.meta.env.VITE_APP_API_URL}/form/${id}`);
     }
   }, [user, id, data, navigate]);
@@ -32,11 +33,7 @@ const ViewForm = () => {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <FormBody
-      data={data}
-      readonly={false}
-      isLogged={user ? true : false}
-    />
+    <FormBody data={data} readonly={false} isLogged={user ? true : false} />
   );
 };
 
