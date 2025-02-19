f128700bfea78e5b8dfc98e30f9341c1c8123997
Krishna Therokar <krishnatherokar@gmail.com>
2025-02-19 22:19:46 +0530

    refractor: correct import paths

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { HashRouter } from "react-router-dom";
-import { UserProvider } from "./context/userContext.jsx";
+import { UserProvider } from "./context/UserContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
