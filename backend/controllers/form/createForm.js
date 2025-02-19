eedf5f07c416a522eba0893681ef80ba0720efca
Krishna Therokar <krishnatherokar@gmail.com>
2025-02-19 14:41:10 +0530

    feat: add create form endpoint

---
+++ backend/controllers/form/createForm.js
@@ -0,0 +1,20 @@
+const formModel = require("../../models/formModel");
+
+const createForm = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const form = await formModel.create({
+      ...req.body,
+      userId: user._id,
+    });
+
+    user.forms.push(form._id);
+    await user.save();
+    
+    res.json(form._id);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = createForm;
--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -4,12 +4,17 @@ const showResponse = require("../controllers/form/showResponse");
 const submitResponse = require("../controllers/form/submitResponse");
 const isLogged = require("../middlewares/isLogged");
 const showFormResponses = require("../controllers/form/showFormResponses");
+const createForm = require("../controllers/form/createForm");
 const formRouter = express.Router();
 
 formRouter.get("/:id", getForm);
-
 formRouter.get("/r/:id", showResponse);
-formRouter.get("/fr/:id", isLogged, showFormResponses);
-formRouter.post("/submit/:id", isLogged, submitResponse);
+
+formRouter.use(isLogged);
+// all the routes that need user data, place them below
+
+formRouter.post("/create", createForm);
+formRouter.post("/submit/:id", submitResponse);
+formRouter.get("/fr/:id", showFormResponses);
 
 module.exports = formRouter;
