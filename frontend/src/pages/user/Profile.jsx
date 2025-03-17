bcdc30bb99faed701a91905201669db8ae04ed4d
Krishna Therokar <krishnatherokar@gmail.com>
2025-03-17 22:21:36 +0530

    refactor(logout): replace axios with useFetch hook

--- frontend/src/pages/user/Profile.jsx
+++ frontend/src/pages/user/Profile.jsx
@@ -1,27 +1,44 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/UserContext";
-import axios from "axios";
 import Profilebody from "../../components/user/Profilebody";
+import useFetch from "../../hooks/useFetch";
+import FullScreen from "../../components/containers/FullScreen";
+import LoadingCard from "../../components/loading/LoadingCard";
+import Error from "../../components/error/Error";
 
 const Profile = () => {
   const { user, setUser } = useContext(UserContext);
-  const logOut = async () => {
-    const response = await axios.delete(
-      `${import.meta.env.VITE_APP_API_URL}/login/remove`,
-      {
-        withCredentials: true,
-      }
-    );
+  const [url, setUrl] = useState(null);
+  const { data: response, loading, error } = useFetch(url, {}, "DELETE");
+  const logOut = () => {
+    setUrl(`${import.meta.env.VITE_APP_API_URL}/login/remove`);
+  };
+
+  useEffect(() => {
     if (response) {
       localStorage.clear();
       setUser(null);
     }
-  };
+  }, [response]);
 
   const props = {
     user,
     logOut,
   };
+
+  if (error)
+    return (
+      <FullScreen>
+        <Error>{error}</Error>
+      </FullScreen>
+    );
+
+  if (url && loading)
+    return (
+      <FullScreen>
+        <LoadingCard>Logging out...</LoadingCard>
+      </FullScreen>
+    );
 
   return <Profilebody {...props} />;
 };
