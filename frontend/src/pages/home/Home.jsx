d34eb57d99a0380ee79a59d3e03665970c46ba78
Krishna Therokar <krishnatherokar@gmail.com>
2025-03-19 17:43:02 +0530

    feat: implement recent forms display on the home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,55 @@
-import Card from "../../components/containers/Card";
-import FullScreen from "../../components/containers/FullScreen";
+import useFetch from "../../hooks/useFetch";
+import { CardSkeleton } from "../../components/loading/FormSkeleton";
+import styles from "./home.module.css";
+import Avatar from "react-avatar";
+import { useNavigate } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
 
 const Home = () => {
+  const { data, loading, error } = useFetch(
+    `${import.meta.env.VITE_APP_API_URL}/form`
+  );
+
+  const navigate = useNavigate();
+
   return (
-    <FullScreen>
-      <Card>Under Construction...</Card>
-    </FullScreen>
+    <>
+      {loading ? (
+        <div className={styles.formListContainer}>
+          Recent Forms:
+          <CardSkeleton />
+        </div>
+      ) : data && !data.empty ? (
+        <div className={styles.formListContainer}>
+          Recent Forms:
+          {data.map((form, i) => {
+            return (
+              <div
+                onClick={() => navigate(`/form/${form._id}`)}
+                className={styles.card}
+                key={i}
+              >
+                <div className={styles.title}>{form.title}</div>
+                <div className={styles.description}>{form.description}</div>
+                <Avatar
+                  className={styles.avatar}
+                  name={form.userInfo.name}
+                  size={24}
+                  round={true}
+                />
+                <span className={styles.username}>{form.userInfo.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+      <div className={styles.footer}>
+        <a href="https://github.com/krishnatherokar/formlabs">
+          <FaGithub />
+          GitHub
+        </a>
+      </div>
+    </>
   );
 };
 export default Home;
